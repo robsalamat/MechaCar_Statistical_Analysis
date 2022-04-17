a93bab338f9584caebbe4a4e513cb5180b4f95bc
Rob Salamat <95326926+robsalamat@users.noreply.github.com>
2022-04-17 10:47:35 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,34 +4,30 @@ library(dplyr)
 #Linear Regression to Predict MPG 
 
 #read file
-MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+mechacar_mpg_df <- read.csv(file='MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
+head(mechacar_mpg_df)
+# multiple linear regression
+lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = mechacar_mpg_df)
+# find the p-value and r-squared
+summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = mechacar_mpg_df))
 
-#perform linear regression
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar)
-
-#perform summary
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar))
 
 #DELIVERABLE 2
 #Create Visualizations for the Trip Analysis
 
 #read file
-Suspension <- read.csv(file='Suspension_coil.csv',check.names=F,stringsAsFactors = F)
+suspension_coil_df <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+head(suspension_coil_df)
+# create a summary dataframe
+total_summary <- suspension_coil_df %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+# create summaries for each lot
+lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
 
-#get a total summary
-summarize_demo <- Suspension %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI)) 
-
-#create a lot summary
-lot_demo <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
 
 #DELIVERABLE 3
 #T-Tests on Suspension Coils
+t.test(suspension_coil_df$PSI, mu=1500)
 
-# Peform t-test across all Lots
-t.test(Suspension$PSI,mu = 1500)
-# Peform t-test on Lot 1
-t.test(subset(Suspension,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
-# Peform t-test on Lot 2
-t.test(subset(Suspension,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
-# Peform t-test on Lot 3
-t.test(subset(Suspension,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
